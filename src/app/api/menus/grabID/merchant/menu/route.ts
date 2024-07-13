4eafd3c8a9429bfab9bf1af430daf690c079117b
rahadianap <rahadianaditya4@outlook.com>
2024-07-13 09:44:12 +0700

    test2

--- src/app/api/menus/grabID/merchant/menu/route.ts
+++ src/app/api/menus/grabID/merchant/menu/route.ts
@@ -10,200 +10,141 @@
 
 // const result = await cetak().catch(error => console.error(error));
 
-const merchantID = process.env.MERCHANT_ID;
-const partnerMerchantID = process.env.PARTNER_MERCHANT_ID;
-
-const response = {
-    "merchantID": merchantID,
-    "partnerMerchantID": partnerMerchantID,
-    "currency": {
-      "code": "IDR",
-      "symbol": "Rp",
-      "exponent": 0
-    },
-    "categories": [
-      {
-        "id": "IDITEDP20220629083554015180",
-        "name": "Snacks & Ice Cream",
-        "availableStatus": "AVAILABLE",
-        "sellingTimeID": "SELL01",
-        "subCategories": [
+const GET = async () => {
+  const merchantID = process.env.MERCHANT_ID;
+  const partnerMerchantID = process.env.PARTNER_MERCHANT_ID;
+  
+  const response = {
+      "merchantID" : merchantID,
+      "partnerMerchantID" : partnerMerchantID,
+      "currency" : {
+          "code" : "IDR",
+          "symbol" : "Rp",
+          "exponent" : 0
+      },
+      "sellingTimes" : [
           {
-            "id": "IDITEDP20220701085346014231",
-            "name": "Chips & Crackers.",
-            "sellingTimeID": "SELL01",
-            "items": [
-              {
-                "id": "016000287914",
-                "name": "BUGLES CARAMEL 6 OZ",
-                "sequence": 0,
-                "availableStatus": "AVAILABLE",
-                "price": 114900,
-                "campaignInfo": null,
-                "description": "BUGLES CARAMEL 6 OZ",
-                "photos": [
-                  "https://developer.grab.com/assets/default-menu/vegetable.png"
-                ],
-                "weight": {
-                  "unit": "per pack",
-                  "value": 1,
-                  "count": 1
-                }
+              "startTime" : "2022-01-09 00:00:00",
+              "endTime" : "2025-01-09 00:00:00",
+              "id" : "SELL01",
+              "name" : "normal",
+              "serviceHours": {
+                  "mon": {
+                      "openPeriodType": "OpenPeriod",
+                      "periods": [
+                          {
+                              "startTime": "08:00",
+                              "endTime": "21:00"
+                          }
+                      ]
+                  },
+                  "tue": {
+                      "openPeriodType": "OpenPeriod",
+                      "periods": [
+                          {
+                              "startTime": "08:00",
+                              "endTime": "21:00"
+                          }
+                      ]
+                  },
+                  "wed": {
+                      "openPeriodType": "OpenPeriod",
+                      "periods": [
+                          {
+                              "startTime": "08:00",
+                              "endTime": "21:00"
+                          }
+                      ]
+                  },
+                  "thu": {
+                      "openPeriodType": "OpenPeriod",
+                      "periods": [
+                          {
+                              "startTime": "08:00",
+                              "endTime": "21:00"
+                          }
+                      ]
+                  },
+                  "fri": {
+                      "openPeriodType": "OpenPeriod",
+                      "periods": [
+                          {
+                              "startTime": "08:00",
+                              "endTime": "21:00"
+                          }
+                      ]
+                  },
+                  "sat": {
+                      "openPeriodType": "OpenPeriod",
+                      "periods": [
+                          {
+                              "startTime": "08:00",
+                              "endTime": "21:00"
+                          }
+                      ]
+                  },
+                  "sun": {
+                      "openPeriodType": "OpenPeriod",
+                      "periods": [
+                          {
+                              "startTime": "08:00",
+                              "endTime": "21:00"
+                          }
+                      ]
+                  }
               }
-            ]
-          },
-        ]
-      },
-    ],
-    "sellingTimes": [
-      {
-        "id": "SELL01",
-        "name": "All Day Selling Hours",
-        "serviceHours": {
-          "mon": {
-            "openPeriodType": "OpenPeriod",
-            "periods": [
-              {
-                "startTime": "00:00",
-                "endTime": "23:59"
-              }
-            ]
-          },
-          "tue": {
-            "openPeriodType": "OpenPeriod",
-            "periods": [
-              {
-                "startTime": "00:00",
-                "endTime": "23:59"
-              }
-            ]
-          },
-          "wed": {
-            "openPeriodType": "OpenPeriod",
-            "periods": [
-              {
-                "startTime": "00:00",
-                "endTime": "23:59"
-              }
-            ]
-          },
-          "thu": {
-            "openPeriodType": "OpenPeriod",
-            "periods": [
-              {
-                "startTime": "00:00",
-                "endTime": "23:59"
-              }
-            ]
-          },
-          "fri": {
-            "openPeriodType": "OpenPeriod",
-            "periods": [
-              {
-                "startTime": "00:00",
-                "endTime": "23:59"
-              }
-            ]
-          },
-          "sat": {
-            "openPeriodType": "OpenPeriod",
-            "periods": [
-              {
-                "startTime": "00:00",
-                "endTime": "23:59"
-              }
-            ]
-          },
-          "sun": {
-            "openPeriodType": "OpenPeriod",
-            "periods": [
-              {
-                "startTime": "00:00",
-                "endTime": "23:59"
-              }
-            ]
           }
-        },
-        "startTime": "1000-01-01 00:00:00",
-        "endTime": "9999-12-31 23:59:59"
-      },
-      {
-        "id": "SELLING-TIME-02",
-        "name": "Alcohol Selling Time",
-        "sequence": 1,
-        "serviceHours": {
-          "mon": {
-            "openPeriodType": "OpenPeriod",
-            "periods": [
-              {
-                "startTime": "08:00",
-                "endTime": "22:00"
-              }
-            ]
-          },
-          "tue": {
-            "openPeriodType": "OpenPeriod",
-            "periods": [
-              {
-                "startTime": "08:00",
-                "endTime": "22:00"
-              }
-            ]
-          },
-          "wed": {
-            "openPeriodType": "OpenPeriod",
-            "periods": [
-              {
-                "startTime": "08:00",
-                "endTime": "22:00"
-              }
-            ]
-          },
-          "thu": {
-            "openPeriodType": "OpenPeriod",
-            "periods": [
-              {
-                "startTime": "08:00",
-                "endTime": "22:00"
-              }
-            ]
-          },
-          "fri": {
-            "openPeriodType": "OpenPeriod",
-            "periods": [
-              {
-                "startTime": "08:00",
-                "endTime": "22:00"
-              }
-            ]
-          },
-          "sat": {
-            "openPeriodType": "OpenPeriod",
-            "periods": [
-              {
-                "startTime": "08:00",
-                "endTime": "22:00"
-              }
-            ]
-          },
-          "sun": {
-            "openPeriodType": "OpenPeriod",
-            "periods": [
-              {
-                "startTime": "08:00",
-                "endTime": "22:00"
-              }
-            ]
+      ],
+      categories: [
+          {
+              "id": "IDITEDP20220629083554015180",
+              "name": "Snacks & Ice Cream",
+              "availableStatus": "AVAILABLE",
+              "sellingTimeID": "SELL01",
+              "subCategories": [
+                  {
+                      "id": "IDITEDP20220701085346014231",
+                      "name": "Chips & Crackers.",
+                      "availableStatus": "AVAILABLE",
+                      "sellingTimeID": "SELL01",
+                      "items": [
+                          {
+                              "id": "016000287914",
+                              "name": "BUGLES CARAMEL 6 OZ",
+                              "nameTranslation": {},
+                              "availableStatus": "AVAILABLE",
+                              "description": "BUGLES CARAMEL 6 OZ",
+                              "descriptionTranslation": {},
+                              "price": 114900,
+                              "photos": [
+                                  "image_url_1",
+                                  "image_url_2"
+                              ],
+                              "specialType": null,
+                              "taxable": false,
+                              "barcode": "GTIN",
+                              "maxStock": 100,
+                              "maxCount": 1,
+                              "weight": {
+                                  "unit": "per pack",
+                                  "value": 1,
+                                  "count": 1
+                              },
+                              "soldByWeight": false,
+                              "sellingUom": {},
+                              "sellingTimeID": "SELL01",
+                              "advancedPricing": {},
+                              "purchasability": {},
+                              "modifierGroups": []
+                          }
+                      ]
+                  }
+              ]
           }
-        },
-        "startTime": "1000-01-01 00:00:00",
-        "endTime": "9999-12-31 23:59:59"
-      }
-    ]
+      ]
   };
 
-const GET = async () => {
-    return Response.json(response);
+  return Response.json(response);
 }
 
 export {GET};
